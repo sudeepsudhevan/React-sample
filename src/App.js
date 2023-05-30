8816520d28a5c12710dd0e473b338e6b5859be91
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-05-30 13:06:47 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 
-function App() {
-  const data = 'something'
-  return (
-   <div>
-    <h1 style={{color:"red"}}>Hello World</h1>
-    <p>it is something {data}</p>
 
-   </div> 
+/**
+ * The function creates a basic counter app with a count variable initialized to 0 and two buttons for
+ * incrementing and decrementing the count.
+ * @returns A React functional component named `App` is being returned. It contains a `div` element
+ * with class name `App`, an `h1` element with text "Counter App", an `h2` element with text "Counter:
+ * 0", and two `button` elements with text "Increment" and "Decrement" respectively.
+ */
+
+import {useState} from 'react';
+
+function App() {
+  const [count,setCount]=useState(0); // [state,setState
+
+
+  const addcount=()=>{
+    setCount(count+1);
+    console.log(count);
+  }
+  const subcount=()=>{
+    setCount(count-1);
+    console.log(count);
+  }
+  return (
+    <div className="App">
+      
+      
+      <button onClick={addcount}>Increment</button>
+      <button onClick={subcount}>Decrement</button>
+      <h1>Counter: {count}</h1>  
+    </div>
+  
+    
   );
 }
-
 export default App;
+
+
