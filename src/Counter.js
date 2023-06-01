1d28b24197df54abd27b94afce84fe1b4890aa49
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-01 17:13:07 +0530

    sample file

--- src/Counter.js
+++ src/Counter.js
@@ -2,20 +2,23 @@ import React,{useState,useEffect} from "react";
 
 function Counter(){
     const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
     useEffect(()=>{
        
         console.log("Component Mounted");
-        console.log("Component Updated"+count);
-        return ()=>{
-            console.log('clean up'+count)
-        } 
-    },[count])
+        console.log("count1: "+count);
+        console.log("count2: "+count2);
+        
+    },[count,count2]) // if we pass empty array then it will run only once when component is mounted
 
     
     return(
         <div>
             <button onClick={()=>setCount(count+1)}>Increment</button> 
             <h1>Hello this is component: {count}</h1>
+
+            <button onClick={()=>setCount2(count2+1)}>Increment</button> 
+            <h1>Hello this is component: {count2}</h1>
         </div>
     )
 }
